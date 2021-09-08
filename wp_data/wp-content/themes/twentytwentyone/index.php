dd26a5530fe2841418b2a058701ca1cf452a3832
Nolwenn Mercier <mercier.nolwenn@yahoo.com>
2021-09-08 10:34:49 +0000

    Update index.php

--- wp_data/wp-content/themes/twentytwentyone/index.php
+++ wp_data/wp-content/themes/twentytwentyone/index.php
@@ -18,7 +18,7 @@ get_header(); ?>
 
 <?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
 	<header class="page-header alignwide">
-		<h1 class="page-title"><?php single_post_title(); ?></h1>
+		<h1 class="page-title"> Hello Arnaud</h1>
 	</header><!-- .page-header -->
 <?php endif; ?>
 
